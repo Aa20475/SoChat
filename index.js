82a5b44ecf7a5008afe7f774a6e24ae6f7d46ae9
Aa20475 <aa20475@gmail.com>
2020-11-23 19:22:46 +0530

    Revamped UI using BootStrap&& Active Users list

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/static'));
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 var users = {}
+var chatHistory = {}
 
 io.on('connection', (socket) => {
 
@@ -18,17 +24,24 @@ io.on('connection', (socket) => {
     users[socket.id] = msg
     console.log(msg+ " joined the chat!");
     io.emit('username', msg);
+    var dat = [];
+    for(var key in users){
+      if(key!=socket.id){
+        dat.push(users[key]);
+      }
+    }
+    io.to(socket.id).emit("activeusers",dat);
   })
   
   socket.on('disconnect', () => {
     console.log(users[socket.id]+' disconnected');
-    io.emit('userleft',users[socket.id]+' left the chat!')
+    io.emit('userleft',users[socket.id])
     delete users[socket.id]
   });
 
 
 });
 
-http.listen(3000, () => {
+http.listen(3000,"192.168.1.3", () => {
   console.log('listening on *:3000');
 });
---
+++ static/script.js
@@ -0,0 +1,60 @@
+$(function () {
+    //initialise socket
+    var socket = io();
+
+    // Username
+    var foo = null
+    while(!foo)[
+      foo = prompt("Username: ")
+    ]
+    $("#dropdownMenuButton").html(foo)
+    socket.emit('username',foo);
+
+    // Signalling
+    $('form').submit(function(e){
+      e.preventDefault(); // prevents page reloading
+      socket.emit('chat message', $('#m').val());
+      $('#m').val('');
+      return false;
+    });
+
+
+    // Signal Handling
+    socket.on('username', function(msg){
+      if(msg!=foo){
+        $("#drops").append(
+          $("<a>").addClass("dropdown-item").attr('id',msg+"id").attr('href',"#").text(msg)
+        )
+      }
+      $('#messages').prepend(
+        $('<li>').addClass("list-group-item").addClass("list-group-item-danger").append(
+          $('<i>').text(msg + " joined the chat!")
+        )
+      )
+    });
+
+    socket.on("activeusers",(dat)=>{
+      for(var d in dat){
+        $("#drops").append(
+          $("<a>").addClass("dropdown-item").attr('id',dat[d]+"id").attr('href',"#").text(dat[d])
+        )
+      }
+    });
+
+    socket.on('userleft', function(msg){
+      $('#messages').prepend(
+        $('<li>').addClass("list-group-item").addClass("list-group-item-danger").append(
+          $('<i>').text(msg+' left the chat!')
+        )
+      )
+      $("#"+msg+"id").remove();
+    });
+
+    socket.on('chat message', function(msg){
+      $('#messages').prepend($('<li>').addClass("list-group-item").append(
+        $('<b>').text(msg['user']+": ")
+      ).append(
+        $('<i>').text(msg['msg'])
+      ))
+    });
+  });
